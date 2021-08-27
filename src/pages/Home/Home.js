20cfb79703e460d5c0a1b3c929b49d035af4fac0
Yiyang-Huang <yiyahuang@student.unimelb.edu.au>
2021-08-27 20:30:56 +1000

    Added material UI core/icons

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,23 @@
+import Button from '@material-ui/core/Button';
+
+import {Link} from 'react-router-dom';
 
 
 function Home() {
   return (
     <div>
-        <p>
-          Hello World!
-        </p>
+      <p>
+        Hello World!
+      </p>
+      <p>
+        This is the homepage.
+      </p>
+      <Link to='/swagger'>
+        <Button variant='contained' color='primary'>
+          API for backend
+        </Button>
+      </Link>
+        
     </div>
   );
 }
