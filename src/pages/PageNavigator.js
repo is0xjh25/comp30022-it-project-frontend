6241b1897faf6445a3bee7df96fcac937bd27318
kzzhe2333 <kzzhe@student.unimelb.edu.au>
2021-09-01 09:40:13 +0800

    Create organization route

--- src/pages/PageNavigator.js
+++ src/pages/PageNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 import Swagger from './Swagger/Swagger';
 import Home from './Home/Home';
+import Org from './Organisation/Organisation';
 
 function PageNavigator() {
     return (
@@ -11,6 +12,9 @@ function PageNavigator() {
             </Route>
             <Route path='/swagger'>
                 <Swagger/>
+            </Route>
+            <Route path ='/org'>
+                <Org/>
             </Route>
         </Switch>
 
