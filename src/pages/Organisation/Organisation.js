8606c591164d04fedd8914b0759d1c54b1857cc0
kzzhe2333 <kzzhe@student.unimelb.edu.au>
2021-09-08 12:25:45 +0800

    Organization Class component version

--- src/pages/Organisation/Organisation.js
+++ src/pages/Organisation/Organisation.js
@@ -1,5 +1,92 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import Box from '@material-ui/core/Box';
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    palette: {
+        background: {
+          default: '#757ce8'
+        }
+      },
+    typography: {
+        button: {
+            textTransform: 'none'
+        },
+    },
+
+    topic: {
+        marginTop: theme.spacing(5),
+        marginLeft: theme.spacing(20),
+        display: 'flex',
+        // border: 10,
+        // alignItems: 'space-around',
+        justifyContent: 'flex-start',
+        component: "h1",
+        color: "primary.main",
+    },
+
+    // 一个box的class，规定box css
+    // 三个不同颜色的css
+    orgGrid: {
+        alignItems: 'flex-start',
+        direction: 'column',
+        justifyContent: 'space-around',
+        warp: 'nowrap',
+        // color: 'rgb(255, 255, 0)',
+        border: 5,
+        borderRadius: 5,
+        bgcolor: 'background.paper',
+        borderColor: 'text.primary',
+        m: 1,
+        // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        padding: '30px',
+    },
+    ownBox: {
+        marginTop: theme.spacing(3),
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        height: 60,
+        border: 4,
+        borderRadius: 8,
+        boxShadow: '0 5px 5px 2px rgba(105, 105, 105, .3)',
+        // m: 1,
+        // borderColor: 'text.primary',
+        // alignItems: 'center',
+        color: theme.palette.success.main,
+    },
+    memberBox: {
+        display: 'flex',
+        flexDirection: 'rous',
+        justifyContent: 'center',
+        height: 60,
+        border: 4,
+        borderRadius: 8,
+        boxShadow: '0 5px 5px 2px rgba(105, 105, 105, .3)',
+        // alignSelf: strentch,
+        // color: theme.palette.info.main,
+    },
+    plusBox: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        height: 60,
+        border: 4,
+        borderRadius: 8,
+        boxShadow: '0 5px 5px 2px rgba(105, 105, 105, .3)',
+        // alignItems: 'center',
+        // color: theme.palette.success.main,
+    },
+}));
+
+// @withStyles(useStyles)
 export default class Organisation extends React.Component {
     // read in the user's organisation info from backend api
     // const classes = useStyles();
@@ -8,8 +95,9 @@ export default class Organisation extends React.Component {
         loading: true,
         organisations: [],
     };
+    
 
-    componentDidMount() {
+    async componentDidMount() {
         // const url;
         // const response = await fetch(url);
         // const data = response.json();
@@ -17,7 +105,7 @@ export default class Organisation extends React.Component {
         // make up some fake data for testing
         const data = [
             {
-                "organization_id":1,
+                "organization_id": 1,
                 "name": "University of Melbourne",
                 "owner_id": 100,
                 "ownership": "own"
@@ -44,11 +132,17 @@ export default class Organisation extends React.Component {
         this.setState({loading: false, organisations: data});
         
     };
+    
 
     // identify organisation data from json
     // create different boxes for different authority levels
     // boxes has link buttons directs to department pages
     render() {
+        
+        // const classes = useStyles();
+
+        // console.log(this.state.loading);
+        // console.log(this.state.organisations);
 
         if (this.state.loading) {
             return <div>loading...</div>
@@ -58,33 +152,61 @@ export default class Organisation extends React.Component {
         //     return <div>No valid organisation</div>
         // }
         
-        const orgs = <div></div>;
-        const result = this.state.organisations.forEach(org => {
-            if (org.ownership === "own") {
-                orgs = orgs +
+        const orgs = [];
+        this.state.organisations.map((org) => {
+            orgs.push(
+                org.ownership === "own" ? 
                     <div key={org.organization_id}>
-                        <div>{org.name}</div>
-                        <div>{org.owner_id}</div>
+                        {/* <div>{org.name}</div>
+                        <div>{org.owner_id}</div> */}
+                        <Grid item alignItems={'center'} xs={8}>
+                        <Box className={classes.ownBox} bgcolor="success.main">
+                            <Button alignItems='center'>
+                                {org.name}
+                            </Button>
+
+                            <IconButton aria-label="personOutlined">
+                                <PersonOutlineOutlinedIcon />
+                            </IconButton>
+                            
+                            <IconButton aria-label="delete">
+                                <DeleteIcon />
+                            </IconButton>
+                        </Box>
+                    </Grid>
                     </div>
-            } else {
-                orgs = orgs +
-                    <div key={org.organization_id}>
-                        <div>{org.name}</div>
-                        <div>{org.owner_id}</div>
-                    </div>
-            }
+                
+                :
+                <div key={org.organization_id}>
+                    {/* <div>{org.owner_id}</div> */}
+                    <Grid item alignItems="center" xs={8}>
+                        <Box className={classes.memberBox} bgcolor="info.main">
+                            <Button>
+                                {org.name}
+                            </Button>
+                        </Box>
+                    </Grid>
+                </div>
+            )
             
         });
 
-        // const plus = <div key={org.organization_id}>
-        //                 <div>{org.name}</div>
-        //                 <div>{org.owner_id}</div>
-        //             </div>
-
         return (
             <div>
-                {result}
-                <div>+</div>
+            <Typography className={classes.topic}>
+                My Orgnizations
+            </Typography>
+            <Grid className={classes.orgGrid} container spacing={5}>
+                {/* {this.state.organisations} */}
+                {orgs}
+                <Grid item xs={8}>
+                    <Box className={classes.plusBox} bgcolor="text.disabled">
+                        <Button>
+                            +
+                        </Button>
+                    </Box>
+                </Grid>
+            </Grid>
             </div>
         )
     }
